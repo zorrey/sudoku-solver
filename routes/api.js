8b9d7e3679c344f7f1d0a98b9a08e48afeb27e9b
Zorrey <102656748+zorrey@users.noreply.github.com>
2022-08-05 14:28:10 +0300

    solver route

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,24 @@ module.exports = function (app) {
     
   app.route('/api/solve')
     .post((req, res) => {
-
+      const { puzzle } = req.body;
+      console.log(req.body);
+      if(!puzzle) { 
+        console.log('error: Required field missing' );
+        return res.json({error: 'Required field missing' });     
+      }
+      if(puzzle.length !=81) { 
+        console.log(' error: Expected puzzle to be 81 characters long ' );
+        return res.json({ error: 'Expected puzzle to be 81 characters long' });     
+      }
+      if(/[^1-9.]/g.test(puzzle)) { 
+        console.log( 'error: Invalid characters in puzzle'  );
+        return res.json({ error: 'Invalid characters in puzzle' });     
+      }
+      let answer = solver.solve(puzzle);
+      if(!answer) {
+        console.log("cannot be solved!!!")
+        return res.json({ error: 'Puzzle cannot be solved' });
+      } 
     });
 };
